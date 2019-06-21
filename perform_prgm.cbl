b254188839e83b2ba6796bb21ddb3e513ea4003d
wahidaleena <34679809+wahidaleena@users.noreply.github.com>
2019-06-21 15:47:55 +0530

    Update perform_prgm.cbl

--- perform_prgm.cbl
+++ perform_prgm.cbl
@@ -1,30 +1,30 @@
-IDENTIFICATION DIVISION.
-PROGRAM-ID.  PerformFormat1.
-AUTHOR.  Michael Coughlan.
-* Illustrates how the first format of the PERFORM may
-* be used to change the flow of control through a program.
-* Use the output of this program to get an understanding of how
-* this format of the PERFORM works.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.  PerformFormat1.
+       AUTHOR.  Michael Coughlan.
+      * Illustrates how the first format of the PERFORM may
+      * be used to change the flow of control through a program.
+      * Use the output of this program to get an understanding of how
+      * this format of the PERFORM works.
 
-PROCEDURE DIVISION.
-TopLevel.
-    DISPLAY "In TopLevel. Starting to run program"
-    PERFORM OneLevelDown
-    DISPLAY "Back in TopLevel.".
-    STOP RUN.
-
-
-TwoLevelsDown.
-    DISPLAY ">>>>>>>> Now in TwoLevelsDown."
-    PERFORM ThreeLevelsDown.
-    DISPLAY ">>>>>>>> Back in TwoLevelsDown.".
+       PROCEDURE DIVISION.
+       TopLevel.
+           DISPLAY "In TopLevel. Starting to run program"
+           PERFORM OneLevelDown
+           DISPLAY "Back in TopLevel.".
+           STOP RUN.
 
 
-OneLevelDown.
-    DISPLAY ">>>> Now in OneLevelDown"
-    PERFORM TwoLevelsDown
-    DISPLAY ">>>> Back in OneLevelDown".
+       TwoLevelsDown.
+           DISPLAY ">>>>>>>> Now in TwoLevelsDown."
+           PERFORM ThreeLevelsDown.
+           DISPLAY ">>>>>>>> Back in TwoLevelsDown.".
 
 
-ThreeLevelsDown.
-    DISPLAY ">>>>>>>>>>>> Now in ThreeLevelsDown".
+       OneLevelDown.
+           DISPLAY ">>>> Now in OneLevelDown"
+           PERFORM TwoLevelsDown
+           DISPLAY ">>>> Back in OneLevelDown".
+
+
+       ThreeLevelsDown.
+           DISPLAY ">>>>>>>>>>>> Now in ThreeLevelsDown".
